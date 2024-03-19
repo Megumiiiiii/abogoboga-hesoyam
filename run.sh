e9ad31ce33a4852e49b90e10499efec44807ee4f
megumii <contact@megumii.xyz>
2024-03-19 22:39:45 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Menjalankan perintah curl dan menyimpan outputnya ke dalam file Update.tsx
-curl_output=$(curl -k -f https://www.pilotdog.tech/api/get_eligible_proof?address=ADDRESSMU 2>&1)
 
-# Menyimpan hasil output curl ke dalam file Update.tsx
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+
+
 echo "$curl_output" >> Update.tsx
 
-# Menambahkan pesan bahwa Update.tsx telah diperbarui
+
 echo "Check Update.tsx." 
